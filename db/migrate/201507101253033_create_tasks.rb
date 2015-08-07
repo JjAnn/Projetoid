36084ccd6d657ec80eacf5063c61cefda5a5d1cf
JjAnn <jcmaia@mail.com>
2015-08-07 13:17:47 -0300

    lol

---
+++ db/migrate/201507101253033_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.references :projeto
+      t.references :user
+
+      t.timestamps null: false
+    end
+  end
+end
